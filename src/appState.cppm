a4b35bcc59150da4dafa2369eceda084773a4ee5
CorpoWolf <gmangavin1@gmail.com>
2026-01-17 02:30:55 -0800

    more merging with DX12 version

--- src/appState.cppm
+++ src/appState.cppm
@@ -3,6 +3,8 @@ module;
 #include <string>
 #include <optional>
 #include <atomic>
+#include <ranges>
+#include <chrono>
 
 export module appState;
 
@@ -15,8 +17,47 @@ export namespace app {
 	bool nineMode{false};
 	bool debugMode{false};
 
-	int port{6454};
+	bool RGBmode{false};
+	bool Unicast{false};
+	bool OpenConnection{false};
+	bool VsyncEnabled{true};
+
+	std::array<char,16> ipStr = {"127.0.0.1"};
+	int ipPort{6454};
 	std::optional<std::string> bindIp;
 
-	
+	int FrameRateSel = 3;
+	using namespace std::chrono_literals;
+
+	struct FpsEntry {
+		const char* label;
+		std::chrono::duration<double, std::milli> ms;
+	};
+
+	constexpr auto FPS_ITEMS = std::to_array<FpsEntry>({
+		{"30", 1s / 30.0},
+		{"60", 1s / 60.0},
+		{"120", 1s / 120.0},
+		{"144", 1s / 144.0},
+		{"165", 1s / 165.0},
+		{"200", 1s / 200.0},
+		{"244", 1s / 244.0},
+		{"300", 1s / 300.0},
+	});
+
+	constexpr auto FPS_LABELS_VIEW = FPS_ITEMS | std::views::transform([](auto&& e) {
+		return e.label;
+	});
+
+	template <typename T, std::size_t N, typename F>
+	constexpr auto to_array(const std::array<T, N>& src, F&& transform) {
+		std::array<std::invoke_result_t<F, T>, N> out{};
+		for (std::size_t i = 0; i < N; ++i)
+			out[i] = transform(src[i]);
+		return out;
+	}
+
+	constexpr auto FPS_LABELS = to_array(FPS_ITEMS, [](const FpsEntry& e) {
+		return e.label;
+	});
 }
--- src/main.cpp
+++ src/main.cpp
@@ -59,13 +59,13 @@ int main(int argc, char* argv[]) {
 				case PORT:
 				if (i + 1 < argc) {
 					try {
-						app::port = std::stoi(argv[++i]);
-						if (app::port < 1 || app::port > 65535) {
+						app::ipPort = std::stoi(argv[++i]);
+						if (app::ipPort < 1 || app::ipPort > 65535) {
 							throw std::out_of_range("Port out of valid range");
 						}
 					} catch (const std::exception& e) {
 						std::cerr << "Error: Missing value for " << arg << " flag." << std::endl;
-						std::cerr << "using default port: " << app::port << std::endl;
+						std::cerr << "using default port: " << app::ipPort << std::endl;
 					}
 					break;
 				}
@@ -172,7 +172,7 @@ int main(int argc, char* argv[]) {
 	glVertexAttribPointer(1, 2, GL_FLOAT, GL_FALSE, 4 * sizeof(float), (void*)(2 * sizeof(float)));
 	glEnableVertexAttribArray(1);
 	
-	SOCKET artNetSocket = setupArtNetSocket(app::port, app::bindIp);
+	SOCKET artNetSocket = setupArtNetSocket(app::ipPort, app::bindIp);
 	
 	std::span<byte> dmxSpan(dmxLogger.dmxData);
 	std::thread artNetThread(receiveArtNetData, artNetSocket, dmxSpan, std::ref(dmxLogger));
@@ -218,28 +218,81 @@ int main(int argc, char* argv[]) {
 		glfwPollEvents();
 
 		ImGui_ImplOpenGL3_NewFrame();
-    ImGui_ImplGlfw_NewFrame();
-    ImGui::NewFrame();
+		ImGui_ImplGlfw_NewFrame();
+		ImGui::NewFrame();
 
     // Build UI
-    ImGui::Begin("DMX Rasterizer UI");
-    ImGui::Text("Hello");
-    ImGui::End();
+		// UI Panel 1
+		ImGui::SetNextWindowPos(ImVec2(10, 10), ImGuiCond_Always);
+		ImGui::SetNextWindowSize(ImVec2(400, 100), ImGuiCond_Always);
+		ImGui::Begin("DmxMainLog", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoResize);
+		ImGui::Text("DMX Rasterizer | v1.0.0");
+		ImGui::Text("FPS: %.1f", ImGui::GetIO().Framerate);
 
-    ImGui::Render();
+		std::string keys_pressed;
+		for (ImGuiKey key = ImGuiKey_NamedKey_BEGIN; key < ImGuiKey_NamedKey_END; key = (ImGuiKey)(key + 1)) {
+			if (ImGui::IsKeyDown(key)) {
+				if (const char* name = ImGui::GetKeyName(key)) {
+					keys_pressed += name;
+					keys_pressed += " ";
+				}
+			}
+		}
+		ImGui::TextWrapped("Keys: %s", keys_pressed.c_str());
+		ImGui::End();
 
-    glfwMakeContextCurrent(uiWindow);
+		// UI Panel 2 -- Settings
+		ImGui::SetNextWindowPos(ImVec2(10, 120), ImGuiCond_Always);
+		ImGui::SetNextWindowSize(ImVec2(400, 145), ImGuiCond_Always);
+		ImGui::Begin("DmxControls", nullptr, ImGuiWindowFlags_NoDecoration | ImGuiWindowFlags_AlwaysAutoResize | ImGuiWindowFlags_NoMove | ImGuiWindowFlags_NoSavedSettings);
+		
+		ImGui::Text("FPS Limit");
+		ImGui::SetNextItemWidth(150.0f);
+		ImGui::BeginDisabled(app::VsyncEnabled);
+		if (ImGui::Combo("##FPS Limit", &app::FrameRateSel, app::FPS_LABELS.data(), as<int>(app::FPS_ITEMS.size()))) {
+			std::wcerr << L"\nfps selected";
+		}
+		ImGui::EndDisabled();
+		
+		ImGui::SameLine(); ImGui::Checkbox("Vsync", &app::VsyncEnabled);
+		ImGui::Text("");
+		ImGui::Text("Network");
+		ImGui::SetNextItemWidth(162.0f);
+		ImGui::InputText("##IPv4", app::ipStr.data(), app::ipStr.size());
+		ImGui::SetNextItemWidth(50.0f);
+		ImGui::SameLine();
+		ImGui::InputInt("##Port", &app::ipPort, 0, 0, ImGuiInputTextFlags_None);
+		
+		ImGui::BeginDisabled(true);
+		if (ImGui::Button("Connect")) {
+			// if (net::Operational()) {
+			// 	net::OpenNetworkSocket(0, state::ipPort);
+			// }
+		}
+		ImGui::EndDisabled();
+		
+		ImGui::BeginDisabled(false);
+		ImGui::SameLine();
+		if (ImGui::Button("Disconnect")) {
+			// net::CloseNetworkSocket();
+		}
+		ImGui::EndDisabled();
+		ImGui::End();
 
-    int fbw = 0, fbh = 0;
-    glfwGetFramebufferSize(uiWindow, &fbw, &fbh);
-    glViewport(0, 0, fbw, fbh);
+		ImGui::Render();
 
-    glClearColor(0.12f, 0.12f, 0.12f, 1.0f);
-    glClear(GL_COLOR_BUFFER_BIT);
+		glfwMakeContextCurrent(uiWindow);
 
-    ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		int fbw = 0, fbh = 0;
+		glfwGetFramebufferSize(uiWindow, &fbw, &fbh);
+		glViewport(0, 0, fbw, fbh);
 
-    glfwSwapBuffers(uiWindow);
+		glClearColor(0.12f, 0.12f, 0.12f, 1.0f);
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+
+		glfwSwapBuffers(uiWindow);
 	}
 	
 	app::running = false;
